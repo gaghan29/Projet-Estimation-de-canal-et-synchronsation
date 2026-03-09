c5663beb84e0249cb9e4b62117961f1406f9bd52
theomalaurieprepa-hue <theo.malaurieprepa@gmail.com>
2026-03-09 16:43:07 +0100

    DSP + animation QPSK

--- src/code_recepteur.m
+++ src/code_recepteur.m
@@ -14,7 +14,7 @@ signal_reel = real(signal_recu);
 signal_complexe = imag(signal_recu);
 
 figure;
-plot(signal_recu);
+plot(signal_recu(1:1000));
 ylabel('Amplitude');
 title('Signal brut');
 grid on;
@@ -31,7 +31,15 @@ ylabel('Amplitude');
 title('Partie imaginaire');
 grid on;
 
+dsp = (abs(fft(signal_recu))).^2;
+figure;
+plot(dsp); xlim([0 100])
 
+for i = 1:700
+    figure(1);
+    plot(signal_reel(i*1000:(i+1)*1000), signal_complexe(i*1000:(i+1)*1000));
+    drawnow limitrate
+end
 %% Votre récepteur
 % En entrée : signal_recu, signal équivalent à rl(kTe) avec Te le temps
 % d'échantillonnage
