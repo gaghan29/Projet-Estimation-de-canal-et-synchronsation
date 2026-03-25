ba62502bae6c719cadaaf5970ff08fae476fd566
TRAVERSINO Eoghan <eoghan.traversino@bordeaux-inp.fr>
2026-03-25 10:41:07 +0100

    Remise en forme du fichier code_recepteur

--- src/code_recepteur.m
+++ src/code_recepteur.m
@@ -19,6 +19,10 @@ Fse = 10; % Echantillonnage
 alpha = 0.3;
 span = 10;
 
+% Paramètres PLL
+alpha_PLL = 0.13;
+beta_PLL = 0.01;
+
 %% Début réception du signal
 
 % Chaine de communication
@@ -32,44 +36,23 @@ signal_ech = signal_filtre(debut_signal:Fse:fin_signal);
 signal_ech = signal_ech/sqrt(mean(abs(signal_ech).^2));
 
 % Bloc PLL
-[theta_est, delta_est] = PLL(signal_ech);
+[theta_est, delta_est] = PLL(signal_ech, alpha_PLL, beta_PLL);
 
 % Bloc de compensation de phase
 signal_comp = signal_ech .* exp(-1j *(theta_est / 4)).*exp(1j*pi/4);
 
 % Bloc de décision
 % Mapping de gray
-%bits_decode = zeros(2, length(signal_comp));
-% demod_part_reelle = real(signal_comp);
-% demod_part_imaginaire = imag(signal_comp);
 
-% Assignation des bits
-% for i = 1:length(signal_comp)
-%     if (demod_part_reelle(i)>0) && (demod_part_imaginaire(i)>0)
-%         bits_decode(1, i) = 1; 
-%         bits_decode(2, i) = 0;
-%     elseif (demod_part_reelle(i) > 0) && (demod_part_imaginaire(i) < 0)
-%         bits_decode(1, i) = 1;
-%         bits_decode(2, i) = 1;
-%     elseif (demod_part_reelle(i) < 0) && (demod_part_imaginaire(i) < 0)
-%         bits_decode(1, i) = 0;
-%         bits_decode(2, i) = 1;
-%     else
-%         bits_decode(1, i) = 0;
-%         bits_decode(2, i) = 0;
-%     end
-% end
-
-% 1. On s'assure que I et Q sont des lignes (1 x 65500)
 Re_positif = (real(signal_comp) > 0).'; 
 Im_positif = (imag(signal_comp) > 0).';
 Re_negatif = (~Re_positif); 
 Im_negatif = (~Im_positif);
 
-% 2. Initialisation
+% Tableau des signaux décodés pour tous les disposition de constéllation possibles
 constellation_variantes = zeros(16, length(signal_comp));
 
-% 3. Remplissage corrigé
+% Remplissage tableau
 constellation_variantes(1:2, :) = [Re_positif ; Im_positif];          % Variante 1
 constellation_variantes(3:4, :) = [Re_positif ; Im_negatif];          % Variante 2
 constellation_variantes(5:6, :) = [Re_negatif ; Im_positif];          % Variante 3
@@ -91,20 +74,21 @@ signal_complexe = imag(signal_recu);
 figure;
 plot(signal_reel);
 ylabel('Amplitude');
-title('Partie réellle');
+title('Partie réellle signal brut');
 grid on;
 
 % Affichage partie imaginaire signal brut
 figure;
 plot(signal_complexe);
 ylabel('Amplitude');
-title('Partie imaginaire');
+title('Partie imaginaire signal brut');
 grid on;
 
 % Affichage constéllation signal brut
 figure;
 plot(signal_recu(1:1000));
-ylabel('Amplitude');
+xlabel('Partie réelle');
+ylabel('Partie imaginaire');
 title('Constéllation signal brut');
 grid on;
 
@@ -118,16 +102,24 @@ grid on;
 %     drawnow limitrate
 % end
 
-p = 4;
-signal_p = signal_recu.^p;
-
 % DSP
-dsp = (abs(fft(signal_p))).^2;
+dsp = (abs(fft(signal_recu))).^2;
 figure;
-plot(dsp); xlim([0 100])
-title('DSP')
+plot(dsp); 
+xlim([0 100]);
+title('Densité spectrale de puissance du signal reçu');
+xlabel('Fréquence');
 ylabel('Amplitude');
 grid on;
+
+% Test début du symbole. Pour déterminer à partir de quelle occurence la première séquence de 10 points débute
+figure;
+for i=1:9
+    subplot(3, 3, i);
+    plot(signal_filtre(i:10:fin_signal), "*");
+    title(['Signal indice ', num2str(i)]);
+    grid on;
+end
 
 % Fréquence et phase estimée
 figure;
@@ -147,6 +139,8 @@ grid on;
 figure;
 plot(signal_comp,'.');
 title('Constéllation apres la PLL');
+xlabel('Partie réelle');
+ylabel('Partie imaginaire');
 grid on;
 
 % % Détermination de la bonne constéllation : 
@@ -239,3 +233,5 @@ colormap gray
 % % figure
 % % imagesc(Img)
 % % colormap gray
+
+
